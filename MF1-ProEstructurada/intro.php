6dd8af2b38ea3636ddadcfb5ffe4f15d39e6efb7
Dani Urbano <daniurbanogarcia26@gmail.com>
2022-09-13 17:47:10 +0200

    "practicando git"

---
+++ MF1-ProEstructurada/intro.php
@@ -0,0 +1,15 @@
+<?php 
+    $texto = "hola";
+    $entero = 2;
+    $decimal = 2.2;
+    $boolean = true;
+
+    if($entero = 2){
+        echo("lol xD");
+    }else{
+        echo("lul xd");
+    }
+
+    echo("<br>");
+    echo($texto." mmgv");
+?>
