910c6f93751659e76a1a420a61599c6da4bc4cb4
Daniel Urbano <daniurbanogarcia26@gmail.com>
2022-10-12 10:58:07 +0200

    casi acabada la pr de js jeje

--- MF6-JavaScript/ej3.js
+++ MF6-JavaScript/ej3.js
@@ -7,27 +7,27 @@ document.getElementById("taula_propietats").innerHTML =
     </tr>
 
     <tr>
-        <td scope="row">Valor maximo que puede tener un numero JS</td>
+        <td>Valor maximo que puede tener un numero JS</td>
         <td>${Number.MAX_SAFE_INTEGER}</td> 
     </tr>
 
     <tr>
-        <td scope="row">Altura total de la pantalla</td>
+        <td>Altura total de la pantalla</td>
         <td>${screen.availHeight}</td>
     </tr>
 
     <tr>
-        <td scope="row">Altura interna de la finestra</td>
+        <td>Altura interna de la finestra</td>
         <td>${window.innerHeight}</td>
     </tr>
 
     <tr>
-        <td scope="row">URL de la web</td>
+        <td>URL de la web</td>
         <td>${window.location}</td>
     </tr>
 
     <tr>
-        <td scope="row">Navegador en el que s'està executant la web</td>
+        <td>Navegador en el que s'està executant la web</td>
         <td>${navigator.userAgent}</td>
     </tr>
 
