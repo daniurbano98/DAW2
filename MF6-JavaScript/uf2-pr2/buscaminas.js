24b95ec29f6f5dcc3914b095036864b34fbe760f
Daniel Urbano <daniurbanogarcia26@gmail.com>
2022-11-03 10:04:30 +0100

    puto js

--- MF6-JavaScript/uf2-pr2/buscaminas.js
+++ MF6-JavaScript/uf2-pr2/buscaminas.js
@@ -1,23 +1,56 @@
 
+let matriz = [];
+let minas = 10;
 
 
 
 function createTable(){
-    let matrix = [];
+    
     let table = document.getElementById("tabla");
 
     for (let i = 0; i < 8; i++) {
-        matrix[i] = new Array(8);   
+        matriz[i] = new Array(8);  //creamos en cada indice un array de 8 
     }
 
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
             let div = document.createElement("div");
             div.className = 'block';
-            matrix[i][j] = div;
-            table.append(matrix[i][j]);
+            matriz[i][j] = div;
+            table.append(matriz[i][j]);
+
+            let random = Math.floor(Math.random() * 4);
+            let posicionRandom = Math.floor(Math.random() * 9);
+            
+                
+            while (random>0) {
+                matriz[i][posicionRandom].className='bomb';
+                --random;
+                       }              
         }
         
     }
+
+   
 }
+
+// function repartirMinas(matriz){
+//     let random = Math.floor(Math.random() * 4);
+//     let posicionRandom = Math.floor(Math.random() * 9);
+    
+
+//     for (let i = 0; i < matriz[i].length; i++) {
+        
+//         do {
+//             matriz[posicionRandom] = ;
+//             --random;
+//             } while (random>0);
+            
+        
+       
+//     }
+       
+              
+    
+// }
 
