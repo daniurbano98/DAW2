9a014284fa544797b1d3876d1eaaacedcc0a9b27
Dani Urbano <daniurbanogarcia26@gmail.com>
2022-11-08 19:14:19 +0100

    puto js

--- MF6-JavaScript/uf2-pr2/buscaminas.js
+++ MF6-JavaScript/uf2-pr2/buscaminas.js
@@ -17,15 +17,16 @@ function createTable(){
             let div = document.createElement("div");
             div.className = 'block';
             matriz[i][j] = div;
-            table.append(matriz[i][j]);       
+            table.append(matriz[i][j]);
+                  
         }
-        
+         
     }
 
    
     repartirBombas(matriz);
     repartirNumeros(matriz);
-    
+      
    
 }
 
@@ -41,7 +42,7 @@ function repartirBombas(matriz){
                 if(minas==0){
                     break;
                 }else{
-                    matriz[i][posicionRandom].innerHTML="bomba";
+                    matriz[i][posicionRandom].setAttribute("data", "bomba");
                     random--;
                     minas--;
                     posicionRandom =  Math.floor(Math.random() * 8);
@@ -58,24 +59,173 @@ function repartirBombas(matriz){
 function repartirNumeros(matriz){
     for (let i = 0; i < matriz.length; i++) {
        for (let x = 0; x < matriz.length; x++) {
-            if(matriz[i][x].innerHTML.indexOf("bomba") !== -1){
+            if(matriz[i][x].hasAttribute("bomba")){
                 continue;
             }else{
                 contador = 0;
-                if(i==0 && x==0){
+                if(i==0 && x==0){ //esquina superior izquierda
                     console.log("entras");
-                    if(matriz[i+1][x].innerHTML.indexOf("bomba") !== -1){ //abajo
+                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
                         contador++;
                     }
-                    if(matriz[i][x+1].innerHTML.indexOf("bomba") !== -1){//derecha
+                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
                         contador++;
                     }
-                    if(matriz[i+1][x+1].innerHTML.indexOf("bomba") !== -1){//diagonal derecha
+                    if(matriz[i+1][x+1].hasAttribute("bomba")){//diagonal derecha
                         contador++;
                     }
                     
-                    matriz[i][x].innerHTML = contador;
+                    matriz[i][x].setAttribute(contador);
+
+                }else if(i==0 && x==7){ //esquina superior derecha
+                    contador = 0;
+                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                        contador++;
+                    }
+                    if(matriz[i+1][x-1].hasAttribute("bomba")){//diagonal izquierda inferior
+                        contador++;
+                    }
+                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
+                        contador++;
+                    }
+                    matriz[i][x].setAttribute(contador);
+
+                }else if(i==7 && x==0){ //esquina inferior izquierda
+                    contador = 0;
+                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                        contador++;
+                    }
+
+                    if(matriz[i-1][x+1].hasAttribute("bomba")){//diagonal derecha
+                        contador++;
+                    }
+
+                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                        contador++;
+                    }
+
+                    matriz[i][x].setAttribute(contador);
+                
+                }else if(i==7 && x==7){
+                    contador = 0;
+
+                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                        contador++;
+                    }
+                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                        contador++;
+                    }
+                    if(matriz[i-1][x-1].hasAttribute("bomba")){//diagonal izquierda superior
+                        contador++;
+                    }
+
+                    matriz[i][x].setAttribute(contador);
+
+                }else if(i==0){
+                    contador = 0;
+
+                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
+                        contador++;
+                    }
+                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                        contador++;
+                    }
+                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                        contador++;
+                    }
+                    
+                    if(matriz[i+1][x-1].hasAttribute("bomba")){//diagonal izquierda inferior
+                        contador++;
+                    }
+                   
+                    if(matriz[i+1][x+1].hasAttribute("bomba")){//diagonal derecha inferior
+                        contador++;
+                    }
+
+                    matriz[i][x].setAttribute(contador);
+
+                }else if(i==7){
+                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                        contador++;
+                    }
+                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                        contador++;
+                    }
+                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                        contador++;
+                    }
+                    if(matriz[i-1][x-1].hasAttribute("bomba")){//diagonal izquierda superior
+                        contador++;
+                    }
+                    if(matriz[i-1][x+1].hasAttribute("bomba")){//diagonal derecha superior
+                        contador++;
+                    }
+
+                    matriz[i][x].setAttribute(contador);
+                
+                }else if(x==0){
+                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                        contador++;
+                    }
+                    if(matriz[i-1][x+1].hasAttribute("bomba")){//diagonal derecha superior
+                        contador++;
+                    }
+                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                        contador++;
+                    }
+                    if(matriz[i+1][x+1].hasAttribute("bomba")){//diagonal derecha inferior
+                        contador++;
+                    }
+                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
+                        contador++;
+                    }
+                    matriz[i][x].setAttribute(contador);
+
+                }else if(x==7){
+                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                        contador++;
+                    }
+                    if(matriz[i-1][x-1].hasAttribute("bomba")){//diagonal izquierda superior
+                        contador++;
+                    }
+                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                        contador++;
+                    }
+                    if(matriz[i+1][x-1].hasAttribute("bomba")){//diagonal izquierda inferior
+                        contador++;
+                    }
+                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
+                        contador++;
+                    }
+                    matriz[i][x].setAttribute(contador);
+                }else{
+                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                        contador++;
+                    }
+                    if(matriz[i-1][x-1].hasAttribute("bomba")){//diagonal izquierda superior
+                        contador++;
+                    }
+                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                        contador++;
+                    }
+                    if(matriz[i+1][x-1].hasAttribute("bomba")){//diagonal izquierda inferior
+                        contador++;
+                    }
+                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
+                        contador++;
+                    }
+                    if(matriz[i+1][x+1].hasAttribute("bomba")){//diagonal derecha inferior
+                        contador++;
+                    }
+                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                        contador++;
+                    }
+                    if(matriz[i-1][x+1].hasAttribute("bomba")){//diagonal derecha superior
+                        contador++;
+                    }
+                    matriz[i][x].setAttribute(contador);
                 }
+
                     
             }
         
@@ -83,4 +233,26 @@ function repartirNumeros(matriz){
         
     }
 }
+
+function pulsado(matriz){
+
+    for (let i = 0; i < matriz.length; i++) {
+       for (let x = 0; x < array.length; x++) {
+            
+        
+       }
+        
+    }
+
+
+
+    if(div.hasAttribute("bomba")){
+        alert("has perdido");
+    }else{
+        atributo = div.getAttribute();
+        div.innerHTML(atributo);
+    }
+}
+
+
 
