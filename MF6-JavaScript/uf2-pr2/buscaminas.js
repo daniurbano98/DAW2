888cfbd9ffb86d893bea84567c5169e8ea2e0273
Daniel Urbano <daniurbanogarcia26@gmail.com>
2022-11-09 09:14:43 +0100

    casi acabado

--- MF6-JavaScript/uf2-pr2/buscaminas.js
+++ MF6-JavaScript/uf2-pr2/buscaminas.js
@@ -16,8 +16,18 @@ function createTable(){
         for (let j = 0; j < matriz[i].length; j++) {
             let div = document.createElement("div");
             div.className = 'block';
+            div.addEventListener("click", function() {
+                let atributo=div.getAttribute("data");
+                if( atributo=="bomba"){
+                    div.innerHTML=div.getAttribute("data");
+                    alert("has perdido");
+                }else{
+                    div.innerHTML=div.getAttribute("data");
+                }
+            });     
             matriz[i][j] = div;
             table.append(matriz[i][j]);
+            
                   
         }
          
@@ -26,9 +36,11 @@ function createTable(){
    
     repartirBombas(matriz);
     repartirNumeros(matriz);
-      
+  
    
 }
+
+
 
 function repartirBombas(matriz){
     
@@ -42,8 +54,7 @@ function repartirBombas(matriz){
                 if(minas==0){
                     break;
                 }else{
-                    matriz[i][posicionRandom].setAttribute("data", "bomba");
-                    
+                    matriz[i][posicionRandom].setAttribute("data", "bomba");            
                     random--;
                     minas--;
                     posicionRandom =  Math.floor(Math.random() * 8);
@@ -67,168 +78,169 @@ function repartirNumeros(matriz){
                 if(i==0 && x==0){ //esquina superior izquierda
                     
                     if(matriz[i+1][x].getAttribute("data")=="bomba"){ //abajo
-                        console.log("entras");
+                        
                         contador++;
                     }
                     if(matriz[i][x+1].getAttribute("data")=="bomba"){//derecha
-                        console.log("entras");
+                        
                         contador++;
                     }
                     if(matriz[i+1][x+1].getAttribute("data")=="bomba"){//diagonal derecha
-                        console.log("entras");
+                       
                         contador++;
                     }
                     
                     matriz[i][x].setAttribute("data",contador);
                    
+                   
 
                 }else if(i==0 && x==7){ //esquina superior derecha
                     contador = 0;
-                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                    if(matriz[i][x-1].getAttribute("data")=="bomba"){//izquierda
+                    contador++;
+                    }
+                    if(matriz[i+1][x-1].getAttribute("data")=="bomba"){//diagonal izquierda inferior
                         contador++;
                     }
-                    if(matriz[i+1][x-1].hasAttribute("bomba")){//diagonal izquierda inferior
+                    if(matriz[i+1][x].getAttribute("data")=="bomba"){ //abajo
                         contador++;
                     }
-                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
-                        contador++;
-                    }
-                    matriz[i][x].setAttribute(contador);
+                    matriz[i][x].setAttribute("data",contador);
 
                 }else if(i==7 && x==0){ //esquina inferior izquierda
                     contador = 0;
-                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                    if(matriz[i-1][x].getAttribute("data")=="bomba"){//arriba
                         contador++;
                     }
 
-                    if(matriz[i-1][x+1].hasAttribute("bomba")){//diagonal derecha
+                    if(matriz[i-1][x+1].getAttribute("data")=="bomba"){//diagonal derecha
                         contador++;
                     }
 
-                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                    if(matriz[i][x+1].getAttribute("data")=="bomba"){//derecha
                         contador++;
                     }
 
-                    matriz[i][x].setAttribute(contador);
+                    matriz[i][x].setAttribute("data",contador);
                 
                 }else if(i==7 && x==7){
                     contador = 0;
 
-                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                    if(matriz[i-1][x].getAttribute("data")=="bomba"){//arriba
                         contador++;
                     }
-                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                    if(matriz[i][x-1].getAttribute("data")=="bomba"){//izquierda
                         contador++;
                     }
-                    if(matriz[i-1][x-1].hasAttribute("bomba")){//diagonal izquierda superior
+                    if(matriz[i-1][x-1].getAttribute("data")=="bomba"){//diagonal izquierda superior
                         contador++;
                     }
 
-                    matriz[i][x].setAttribute(contador);
+                    matriz[i][x].setAttribute("data",contador);
 
                 }else if(i==0){
                     contador = 0;
 
-                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
+                    if(matriz[i+1][x].getAttribute("data")=="bomba"){ //abajo
                         contador++;
                     }
-                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                    if(matriz[i][x+1].getAttribute("data")=="bomba"){//derecha
                         contador++;
                     }
-                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                    if(matriz[i][x-1].getAttribute("data")=="bomba"){//izquierda
                         contador++;
                     }
                     
-                    if(matriz[i+1][x-1].hasAttribute("bomba")){//diagonal izquierda inferior
+                    if(matriz[i+1][x-1].getAttribute("data")=="bomba"){//diagonal izquierda inferior
                         contador++;
                     }
                    
-                    if(matriz[i+1][x+1].hasAttribute("bomba")){//diagonal derecha inferior
+                    if(matriz[i+1][x+1].getAttribute("data")=="bomba"){//diagonal derecha inferior
                         contador++;
                     }
 
-                    matriz[i][x].setAttribute(contador);
+                    matriz[i][x].setAttribute("data",contador);
 
                 }else if(i==7){
-                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                    if(matriz[i-1][x].getAttribute("data")=="bomba"){//arriba
                         contador++;
                     }
-                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                    if(matriz[i][x+1].getAttribute("data")=="bomba"){//derecha
                         contador++;
                     }
-                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                    if(matriz[i][x-1].getAttribute("data")=="bomba"){//izquierda
                         contador++;
                     }
-                    if(matriz[i-1][x-1].hasAttribute("bomba")){//diagonal izquierda superior
+                    if(matriz[i-1][x-1].getAttribute("data")=="bomba"){//diagonal izquierda superior
                         contador++;
                     }
-                    if(matriz[i-1][x+1].hasAttribute("bomba")){//diagonal derecha superior
+                    if(matriz[i-1][x+1].getAttribute("data")=="bomba"){//diagonal derecha superior
                         contador++;
                     }
 
-                    matriz[i][x].setAttribute(contador);
+                    matriz[i][x].setAttribute("data",contador);
                 
                 }else if(x==0){
-                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                    if(matriz[i-1][x].getAttribute("data")=="bomba"){//arriba
                         contador++;
                     }
-                    if(matriz[i-1][x+1].hasAttribute("bomba")){//diagonal derecha superior
+                    if(matriz[i-1][x+1].getAttribute("data")=="bomba"){//diagonal derecha superior
                         contador++;
                     }
-                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                    if(matriz[i][x+1].getAttribute("data")=="bomba"){//derecha
                         contador++;
                     }
-                    if(matriz[i+1][x+1].hasAttribute("bomba")){//diagonal derecha inferior
+                    if(matriz[i+1][x+1].getAttribute("data")=="bomba"){//diagonal derecha inferior
                         contador++;
                     }
-                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
+                    if(matriz[i+1][x].getAttribute("data")=="bomba"){ //abajo
                         contador++;
                     }
-                    matriz[i][x].setAttribute(contador);
+                    matriz[i][x].setAttribute("data",contador);
 
                 }else if(x==7){
-                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                    if(matriz[i-1][x].getAttribute("data")=="bomba"){//arriba
                         contador++;
                     }
-                    if(matriz[i-1][x-1].hasAttribute("bomba")){//diagonal izquierda superior
+                    if(matriz[i-1][x-1].getAttribute("data")=="bomba"){//diagonal izquierda superior
                         contador++;
                     }
-                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                    if(matriz[i][x-1].getAttribute("data")=="bomba"){//izquierda
                         contador++;
                     }
-                    if(matriz[i+1][x-1].hasAttribute("bomba")){//diagonal izquierda inferior
+                    if(matriz[i+1][x-1].getAttribute("data")=="bomba"){//diagonal izquierda inferior
                         contador++;
                     }
-                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
+                    if(matriz[i+1][x].getAttribute("data")=="bomba"){ //abajo
                         contador++;
                     }
-                    matriz[i][x].setAttribute(contador);
+                    matriz[i][x].setAttribute("data",contador);
                 }else{
-                    if(matriz[i-1][x].hasAttribute("bomba")){//arriba
+                    if(matriz[i-1][x].getAttribute("data")=="bomba"){//arriba
                         contador++;
                     }
-                    if(matriz[i-1][x-1].hasAttribute("bomba")){//diagonal izquierda superior
+                    if(matriz[i-1][x-1].getAttribute("data")=="bomba"){//diagonal izquierda superior
                         contador++;
                     }
-                    if(matriz[i][x-1].hasAttribute("bomba")){//izquierda
+                    if(matriz[i][x-1].getAttribute("data")=="bomba"){//izquierda
                         contador++;
                     }
-                    if(matriz[i+1][x-1].hasAttribute("bomba")){//diagonal izquierda inferior
+                    if(matriz[i+1][x-1].getAttribute("data")=="bomba"){//diagonal izquierda inferior
                         contador++;
                     }
-                    if(matriz[i+1][x].hasAttribute("bomba")){ //abajo
+                    if(matriz[i+1][x].getAttribute("data")=="bomba"){ //abajo
                         contador++;
                     }
-                    if(matriz[i+1][x+1].hasAttribute("bomba")){//diagonal derecha inferior
+                    if(matriz[i+1][x+1].getAttribute("data")=="bomba"){//diagonal derecha inferior
                         contador++;
                     }
-                    if(matriz[i][x+1].hasAttribute("bomba")){//derecha
+                    if(matriz[i][x+1].getAttribute("data")=="bomba"){//derecha
                         contador++;
                     }
-                    if(matriz[i-1][x+1].hasAttribute("bomba")){//diagonal derecha superior
+                    if(matriz[i-1][x+1].getAttribute("data")=="bomba"){//diagonal derecha superior
                         contador++;
                     }
-                    matriz[i][x].setAttribute(contador);
+                    matriz[i][x].setAttribute("data",contador);
                 }
 
                     
@@ -239,24 +251,9 @@ function repartirNumeros(matriz){
     }
 }
 
-function asignarClicado(matriz){
 
-    for (let i = 0; i < matriz.length; i++) {
-       for (let x = 0; x < array.length; x++) {
-            matriz[i][j].addEventListener("click",click())
-       }
-        
-    }
-}
 
-function click(){
-    if(div.hasAttribute("bomba")){
-        alert("has perdido");
-    }else{
-        atributo = div.getAttribute();
-        div.innerHTML(atributo);
-    }
-}
+
 
    
 
