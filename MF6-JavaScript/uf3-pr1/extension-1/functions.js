ddfa0e428f7ddd0bb3e7c70cb6dcc1484bb6762b
Daniel Urbano <daniurbanogarcia26@gmail.com>
2023-02-02 20:39:14 +0100

    .

--- MF6-JavaScript/uf3-pr1/extension-1/functions.js
+++ MF6-JavaScript/uf3-pr1/extension-1/functions.js
@@ -75,9 +75,10 @@ function exec_delete_img(){
 
 
 let bt4 = document.getElementById("btn_show_ocult_password");
-bt4.addEventListener("click",click_show_password);
-
-
+bt4.addEventListener("click",click_show_password);let show = true;
+let arrayInputPassword = [];
+arrayInputPassword = document.querySelectorAll('input[type="password"]')
+console.log(arrayInputPassword);
 
 
 function click_show_password(evt){
@@ -86,27 +87,32 @@ function click_show_password(evt){
     chrome.scripting.executeScript({
         target:{"tabId":tabId},
         func:exec_show_ocult_password,
-        args:[]
+        args:[arrayInputPassword,show]
     });
 }
 
-function exec_show_ocult_password(){
-    var input = document.getElementsByTagName("input");
-        
-        if(input.hasOwnProperty('is_pass') && input.is_pass==true){
-            
+
+
+function exec_show_ocult_password(arrayInputPassword, show){
+    for (var i = 0; i < arrayInputPassword.length; i++) {
+        var input = arrayInputPassword[i];
+        if (show) {
+          // Show password
+          input.type = "text";
+          input.setAttribute("is_pass", "true");
+        } else {
+          // Hide password
+          input.type = "password";
+          input.setAttribute("is_pass", "false");
         }
-        for (let i = 0; i < input.length; i++) {
-            if (input[i].type == "password") {
-                if (!input[i].hasOwnProperty("isPass")) {
-                    input[i].is_pass = true;
-                    console.log(input[i].is_pass);
-                  }
-                input[i].type = "text";
-            }
-        }
-      
-}
+      }
+      show = !show;
+    }
+
+    
+    
+
+
 
 
 
