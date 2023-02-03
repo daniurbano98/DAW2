b506a9eeaea0afde0001cf93f8ffe00e4c2f7d28
Daniel Urbano <daniurbanogarcia26@gmail.com>
2023-02-03 15:03:37 +0100

    .

--- PR6-PHP/sturia/app/Http/Controllers/PlaneController.php
+++ PR6-PHP/sturia/app/Http/Controllers/PlaneController.php
@@ -11,6 +11,7 @@ use PhpParser\Node\Stmt\Foreach_;
 
 class PlaneController extends Controller
 {
+    
     public function index()
     {
         $path = storage_path('/json/planes.json');
@@ -21,26 +22,47 @@ class PlaneController extends Controller
     public function edit($id)
     {
         $path = storage_path('/json/planes.json');
-        $aviones = json_decode(file_get_contents($path), true);                                                           
-        $avion = collect($aviones)->firstWhere('id', $id);
-        return view('editPlane', compact('avion'));
+        $aviones = json_decode(file_get_contents($path), true); 
+        
+
+        foreach($aviones as $avion){
+            if($avion['id']==$id){
+                $avion = collect($aviones)->firstWhere('id', $id);
+                return view('editPlane', compact('avion'));
+            }
+        }
+
+        $log = new Logger('PlaneController');
+        $log->pushHandler(new StreamHandler(storage_path('/logs/laravel.log')));
+        $log->info('El avión al que se ha intentado acceder para editar no existe');
+        return view('errorView');  
         
     }
 
-    public function update($idVuelo, Request $request)
+    public function update($id, Request $request)
     {
-        $idVuelo = (int)($idVuelo);
+        $id = (int)($id);
         $path = storage_path('/json/planes.json'); //recogemos el json 
         $aviones = json_decode(file_get_contents($path), true); // lo convertimos en array
-        $index = collect($aviones)->search(function ($item) use ($idVuelo) { // busca en el array $aviones un elemento cuyo valor de la clave "id" sea igual a $idVuelo
-        });
 
-        $aviones[$index] = array_merge($aviones[$index], $request->all()); //realizamos el cambio en el array
-        file_put_contents($path, json_encode($aviones)); //reescribimos el json
+
+        foreach($aviones as $avion){
+            if($avion['id']==$id){
+                $index = collect($aviones)->search(function ($item) use ($id) { // busca en el array $aviones un elemento cuyo valor de la clave "id" sea igual a $idVuelo
+                });
+                $aviones[$index] = array_merge($aviones[$index], $request->all()); //realizamos el cambio en el array
+                file_put_contents($path, json_encode($aviones)); //reescribimos el json
+                $log = new Logger('PlaneController');
+                $log->pushHandler(new StreamHandler(storage_path('/logs/laravel.log')));
+                $log->info('Se ha editado el avión con número ' . $id. ' de id');
+                return redirect('/sturia/planes');        
+            }
+        }
+
         $log = new Logger('PlaneController');
         $log->pushHandler(new StreamHandler(storage_path('/logs/laravel.log')));
-         $log->info('Se ha editado el avión con número ' . $idVuelo . ' de id');
-        return redirect('/sturia/planes');
+        $log->info('El avión que se ha intentado editar no existe');
+        return view('errorView'); 
         
     }
 
@@ -50,18 +72,31 @@ class PlaneController extends Controller
 
         $path = storage_path('/json/planes.json');
         $aviones = json_decode(file_get_contents($path), true);
-        $index = collect($aviones)->search(function ($item) use ($id) {
-            return $item['id'] == $id;
-        });
+       
+
+        foreach($aviones as $avion){
+            if($avion['id']==$id){
+                $index = collect($aviones)->search(function ($item) use ($id) {
+                    return $item['id'] == $id;
+                }); 
+                unset($aviones[$index]); //lo borramos del array
+                $aviones = array_values($aviones);
+                file_put_contents($path, json_encode($aviones)); //reescribimos el json
+                $log = new Logger('PlaneController');
+                $log->pushHandler(new StreamHandler(storage_path('/logs/laravel.log')));
+                $log->info('Se ha borrado el avión con número ' . $id . ' de id');
         
-        unset($aviones[$index]); //lo borramos del array
-        $aviones = array_values($aviones);
-        file_put_contents($path, json_encode($aviones)); //reescribimos el json
+                return redirect('/sturia/planes'); 
+                
+            }
+        }
+
+        
         $log = new Logger('PlaneController');
         $log->pushHandler(new StreamHandler(storage_path('/logs/laravel.log')));
-        $log->info('Se ha borrado el avión con número ' . $id . ' de id');
-
-        return redirect('/sturia/planes');
+        $log->info('El avion que se ha intentado eliminar no existe');
+        return view('errorView'); 
+        
         
     }
 }
