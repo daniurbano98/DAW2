ef861c35d7563cc50399844b9da0edec40f96bfb
Daniel Urbano <daniurbanogarcia26@gmail.com>
2023-02-14 21:22:54 +0100

    acabado js

--- MF6-JavaScript/uf3-pr1/extension-2/functions.js
+++ MF6-JavaScript/uf3-pr1/extension-2/functions.js
@@ -6,96 +6,103 @@ chrome.tabs.query({ active: true, currentWindow: true },
     }
 );
 
-//primer boton
-let btn = document.getElementById("btn_info_img");
-btn.addEventListener("click", click_show_alt);
 
-function click_show_alt(evt) {
+let btn = document.getElementById("btn_start");
+btn.addEventListener("click", click_show_menu);
+
+function click_show_menu(evt) {
     chrome.scripting.executeScript({
         target: { "tabId": tabId },
-        func: exec_show_alt,
+        func: amazonExtension,
         args: []
     });
 }
 
-function exec_show_alt(){
-    const images = document.querySelectorAll("img");
-    images.forEach(img => {
-        img.addEventListener("mouseover", event => {
-          const altText = event.target.getAttribute("alt");
-          const textNode = document.createTextNode(altText);
-          const div = document.createElement("div");
-          div.appendChild(textNode);
-          div.style.position = "absolute";
-          div.style.top = `${event.clientY + 20}px`;
-          div.style.left = `${event.clientX + 20}px`;
-          document.body.appendChild(div);
-        });
-      
-        img.addEventListener("mouseout", event => {
-          const altText = event.target.getAttribute("alt");
-          const divs = document.querySelectorAll("div");
-          const div = Array.from(divs).find(el => el.textContent === altText);
-          div.remove();
-        });
-      });
-}
+function amazonExtension() {
+    const div = document.createElement("div");
 
-//segundo boton
-let btn2 = document.getElementById("btn_precio_mas_bajo");
-btn2.addEventListener("click", click_precio_mas_bajo);
+    div.style.position = "fixed";
+    div.style.right = "0";
+    div.style.top = "50%";
+    div.style.transform = "translateY(50%, -50%)";
+    div.style.width = "400px";
+    div.style.height = "300px";
+    div.style.backgroundColor = "yellow";
+    div.style.textAlign = "center";
+    div.style.margin = "auto";
+    div.style.display = 'flex';
+    div.style.alignItems = 'center';
+    div.style.justifyContent = 'center';
+    div.setAttribute('id', 'myDiv');
+    document.body.appendChild(div);
+    div.innerHTML = `
+  <button id="low_price">Precio mas bajo</button>
+  <button id="info_img">Alt img</button>
+  <button id="close_btn">Cerrar menu</button>
+`;
 
-function click_precio_mas_bajo(evt) {
-    chrome.scripting.executeScript({
-        target: { "tabId": tabId },
-        func: exec_precio_mas_bajo,
-        args: []
+    let cerrarMenu = document.querySelector("#close_btn");
+    let precioMasBajo = document.querySelector("#low_price");
+    let infoImg = document.querySelector("#info_img");
+
+    cerrarMenu.addEventListener("click", function () {
+        const div = document.querySelector('#myDiv');
+        div.remove();
+    });
+
+    precioMasBajo.addEventListener("click", function () {
+        const precios = document.querySelectorAll("._cDEzb_p13n-sc-price_3mJ9Z");
+        let precioMasPequeno = Infinity;
+        let productoMasBarato;
+
+        for (const precio of precios) {
+            //parseFloat para convertir el valor en numero y que se pueda comparar
+            const valor = parseFloat(precio.textContent.replace("$", ""));
+            if (valor < precioMasPequeno) {
+                precioMasPequeno = valor;
+                productoMasBarato = precio.parentNode; //parentNode para obtener el elemento que contiene el precio  del producto, y luego se resalta ese elemento
+            }
+        }
+        // Resalta el producto más barato
+        productoMasBarato.style.backgroundColor = "yellow";
+        productoMasBarato.style.color = "black";
+        //  scrollIntoView para mostrar el producto
+        productoMasBarato.scrollIntoView({ behavior: "smooth", block: "center" });
+        
+    });
+
+    infoImg.addEventListener("click", function () {
+        // Obtenemos todas las imágenes de la página
+        const imagenes = document.getElementsByTagName("img");
+
+        // Recorremos todas las imágenes y agregamos eventos a cada una
+        for (let i = 0; i < imagenes.length; i++) {
+        // Creamos un elemento <span> para mostrar el texto del atributo "alt"
+        const textoAlt = document.createElement("span");
+        textoAlt.style.display = "none";
+        textoAlt.style.position = "absolute";
+        textoAlt.style.backgroundColor = "black";
+        textoAlt.style.color = "white";
+        textoAlt.style.padding = "5px";
+        textoAlt.style.zIndex = "9999";
+        textoAlt.textContent = imagenes[i].getAttribute("alt");
+
+        // Agregamos el elemento <span> como hermano adyacente de la imagen
+        imagenes[i].parentNode.insertBefore(textoAlt, imagenes[i].nextSibling);
+
+        // Agregar el evento "mouseover" para mostrar el texto del atributo "alt"
+        imagenes[i].addEventListener("mouseover", () => {
+            textoAlt.style.display = "block";
+        });
+
+        // Agregar el evento "mouseout" para ocultar el texto del atributo "alt"
+        imagenes[i].addEventListener("mouseout", () => {
+            textoAlt.style.display = "none";
+        });
+        }
     });
 }
 
-function exec_precio_mas_bajo(){
-  
-    const elements = document.querySelectorAll('span');
-
-    // Crear un array para almacenar los elementos que contienen "price" en su clase
-    let lowestElement = null;
-    let elementsWithPriceClassStartingWithUnderscore=[];
-
-    elements.forEach(element => {
-    if (element.classList.value.includes('price') && element.classList.value[0] === '_') {
-        // Almacenar el elemento en el array
-        elementsWithPriceClassStartingWithUnderscore.push(element);
-      }
-    });
-
-    // Recorrer todos los elementos
-    for (const element of elementsWithPriceClassStartingWithUnderscore) {
-    // Obtener la clase del elemento como un string
-    const classString = element.classList.value;
-
-    // Crear una expresión regular para buscar solo los números en la clase
-    const regex = /\d+/g;
-
-    // Usar la expresión regular para buscar los números en la clase
-    const numbers = classString.match(regex);
-
-    // Unir los números encontrados en una sola cadena
-    const numbersString = numbers.join('');
-
-    // Convertir la cadena de números en un número
-    const number = parseInt(numbersString);
-
-    // Comparar el número con la clase más baja actual
-    if (!lowestElement || number < lowestElement) {
-        lowestElement = number;
-        // Guardar el elemento con la clase más baja
-        lowestElement = element;
-    }
-    }
-
-    // Realizar alguna acción con el elemento con la clase más baja
-    lowestElement.style.backgroundColor = 'yellow';
-    lowestElement.scrollIntoView();
 
 
-    }
+
